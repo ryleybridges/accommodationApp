eea8c4a64d519cc802a13354613e1b09ca97378e
ryleybridges <rye.brid@gmail.com>
2019-06-25 11:47:56 +1200

    Updated

--- js/script.js
+++ js/script.js
@@ -135,27 +135,27 @@ function formPage(){
   var formHTML = document.getElementById('mainPageContainer');
   formHTML.innerHTML += ('<div class="row">');
   formHTML.innerHTML += ('<div class="col">');
-  formHTML.innerHTML += ('<h4 class="text-center mt-5">Where are you headed?</h4> <!-- Use autocomplete from Google API -->')
+  formHTML.innerHTML += ('<h4 class="text-center mt-2">Where are you headed?</h4> <!-- Use autocomplete from Google API -->')
   formHTML.innerHTML += ('<input type="text" class="form-control">');
   formHTML.innerHTML += ('</div>');
   formHTML.innerHTML += ('<div class="col">');
-  formHTML.innerHTML += ('<h4 class="text-center mt-5">When are you heading there?</h4> <!-- Use datepicker from JQuery UI -->');
-  formHTML.innerHTML += ('<input type="text" class="form-control">');
-  formHTML.innerHTML += ('</div>');
-  formHTML.innerHTML += ('</div>');
-  formHTML.innerHTML += ('<div class="row">');
-  formHTML.innerHTML += ('<div class="col">');
-  formHTML.innerHTML += ('<h4 class="text-center mt-5">How many people?</h4>');
-  formHTML.innerHTML += ('<input type="text" class="form-control">');
-  formHTML.innerHTML += ('</div>');
-  formHTML.innerHTML += ('<div class="col">');
-  formHTML.innerHTML += ('<h4 class="text-center mt-5">How many nights?</h4>');
+  formHTML.innerHTML += ('<h4 class="text-center mt-2">When are you heading there?</h4> <!-- Use datepicker from JQuery UI -->');
   formHTML.innerHTML += ('<input type="text" class="form-control">');
   formHTML.innerHTML += ('</div>');
   formHTML.innerHTML += ('</div>');
   formHTML.innerHTML += ('<div class="row">');
   formHTML.innerHTML += ('<div class="col">');
-  formHTML.innerHTML += ('<button type="button" class="btn btn-dark btn-lg mt-5 d-flex justify-content-center" onclick="resultsPage();">Find Accommodation</button>');
+  formHTML.innerHTML += ('<h4 class="text-center mt-2">How many people?</h4>');
+  formHTML.innerHTML += ('<input type="text" class="form-control">');
+  formHTML.innerHTML += ('</div>');
+  formHTML.innerHTML += ('<div class="col">');
+  formHTML.innerHTML += ('<h4 class="text-center mt-2">How many nights?</h4>');
+  formHTML.innerHTML += ('<input type="text" class="form-control">');
+  formHTML.innerHTML += ('</div>');
+  formHTML.innerHTML += ('</div>');
+  formHTML.innerHTML += ('<div class="row">');
+  formHTML.innerHTML += ('<div class="col">');
+  formHTML.innerHTML += ('<button type="button" class="btn btn-dark btn-lg mt-2 d-flex justify-content-center" onclick="resultsPage();">Find Accommodation</button>');
   formHTML.innerHTML += ('</div>');
   formHTML.innerHTML += ('</div>');
 }
