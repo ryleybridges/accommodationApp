b3d374dd793f9e5041f7ee2bb1df709727c89db3
ryleybridges <rye.brid@gmail.com>
2019-06-25 08:56:16 +1200

    Updated

--- js/script.js
+++ js/script.js
@@ -1,1 +1,56 @@
-
+accommodationOptions = [
+  {
+    id: 1,
+    title: 'Novotel Wellington',
+    type: 'Hotel',
+    lat: -41.283823,
+    lng: 174.774631,
+    description: 'Novotel Wellington is a 4.5 star hotel located in the heart of the CBD, close to iconic attractions - Te Papa, Cable Car, Botanical Gardens and the Waterfront. Our modern accommodation rooms have a pop of colour and are designed with guest comfort in mind. All rooms feature smart flat screen televisions, working desks and plenty of natural light.',
+    image: 'novotel.jpg'
+  },
+  {
+    id: 2,
+    title: 'InterContinetal Wellington',
+    type: 'Hotel',
+    lat: -41.284636,
+    lng: 174.776942,
+    description: 'The 5-star InterContinental Wellington is located by the harbor waterfront, just 5 mi from Wellington International Airport. It offers an exclusive club lounge, a fitness center, 2 restaurants and a bar. The elegant guest rooms are spacious and well-furnished. All rooms feature a TV with cable channels and a large bathroom with modern amenities and bathrobes. Some rooms include access to the club lounge.',
+    image: 'intercontinetal-wellington.jpg'
+  },
+  {
+    id: 3,
+    title: 'Boulcott Suites',
+    type: 'Hotel',
+    lat: -41.289109,
+    lng: 174.773975,
+    description: 'Centrally located in Wellington, Boulcott Suites is a unique 5-star apartment hotel that has been designed and fully furnished with elegance and style. From studio apartments and spacious single-level suites to fully-equipped executive four bedroom townhouses, Boulcott Suites is ideal if you want quality and space. Some apartments also feature balconies.',
+    image: 'boulcott.jpg'
+  },
+  {
+    id: 4,
+    title: 'YHA Wellington Backpacker Hostel',
+    type: 'Hostel',
+    lat: -41.293066,
+    lng: 174.783989,
+    description: 'Located in the very center of Wellington, this award-winning hostel features 2 fully equipped kitchens and dining areas and a spacious guest lounge room with TV projection screen and free movie hire. 2GB of free WiFi is available each day for all guests.',
+    image: ''
+  },
+  {
+    id: 5,
+    title: 'Nomads Capital Hostel Wellington',
+    type: 'Hostel',
+    lat: -41.289576,
+    lng: 174.777058,
+    description: 'Nomads Capital is a 5 star flashpacker hostel right in the heart of Wellington. Five minutes walk from the vibrant Cuba Street, bustling Courtenay Place, beautiful waterfront and award winning Te Papa Museum. With a range of rooms to suit every traveller and budget, and our own on-site bar and Travel Desk, we can take care of your every need while you are in town. This really is the perfect place to stay!',
+    image: 'nomads.jpg'
+  },
+  {
+    id: 6,
+    title: 'The Marion Hostel',
+    type: 'Hostel',
+    lat: ,
+    lng: ,
+    description: '',
+    image: ''
+  }
+]
