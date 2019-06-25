462fbc8dd67496452af622580a00ed26a53b1e6d
ryleybridges <rye.brid@gmail.com>
2019-06-25 14:31:03 +1200

    Updated

--- js/script.js
+++ js/script.js
@@ -133,32 +133,14 @@ startingPage();
 function formPage(){
   $('#mainPageContainer').html('');
   var formHTML = document.getElementById('mainPageContainer');
-  formHTML.innerHTML += ('<div class="row">');
-  formHTML.innerHTML += ('<div class="col">');
-  formHTML.innerHTML += ('<h4 class="text-center mt-2">Where are you headed?</h4> <!-- Use autocomplete from Google API -->')
-  formHTML.innerHTML += ('<input type="text" class="form-control">');
-  formHTML.innerHTML += ('</div>');
-  formHTML.innerHTML += ('<div class="col">');
-  formHTML.innerHTML += ('<h4 class="text-center mt-2">When are you heading there?</h4> <!-- Use datepicker from JQuery UI -->');
-  formHTML.innerHTML += ('<input type="text" class="form-control">');
-  formHTML.innerHTML += ('</div>');
-  formHTML.innerHTML += ('</div>');
-  formHTML.innerHTML += ('<div class="row">');
-  formHTML.innerHTML += ('<div class="col">');
-  formHTML.innerHTML += ('<h4 class="text-center mt-2">How many people?</h4>');
-  formHTML.innerHTML += ('<input type="text" class="form-control">');
-  formHTML.innerHTML += ('</div>');
-  formHTML.innerHTML += ('<div class="col">');
-  formHTML.innerHTML += ('<h4 class="text-center mt-2">How many nights?</h4>');
-  formHTML.innerHTML += ('<input type="text" class="form-control">');
-  formHTML.innerHTML += ('</div>');
-  formHTML.innerHTML += ('</div>');
-  formHTML.innerHTML += ('<div class="row">');
-  formHTML.innerHTML += ('<div class="col">');
-  formHTML.innerHTML += ('<button type="button" class="btn btn-dark btn-lg mt-2 d-flex justify-content-center" onclick="resultsPage();">Find Accommodation</button>');
-  formHTML.innerHTML += ('</div>');
-  formHTML.innerHTML += ('</div>');
+  formHTML.innerHTML += ('<div class="row"><div class="col"><h4 class="text-center mt-5">Where are you headed?</h4> <!-- Use autocomplete from Google API --><input type="text" class="form-control"></div><div class="col"><h4 class="text-center mt-5" id="datepicker">When are you heading there?</h4> <!-- Use datepicker from JQuery UI --><input type="text" class="form-control"></div></div>');
+  formHTML.innerHTML += ('<div class="row"><div class="col"><h4 class="text-center mt-5">How many people?</h4><input type="text" class="form-control"></div><div class="col"><h4 class="text-center mt-5">How many nights?</h4><input type="text" class="form-control"></div></div>');
+  formHTML.innerHTML += ('<div class="row"><div class="col"><button type="button" class="btn btn-dark btn-lg mt-5 d-flex justify-content-center" onclick="resultsPage();">Next</button></div></div>');
 }
+
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
 
 // function resultsPage(){
 //   $('#mainPageContainer').html('');
